48023bb7f5535d5d98057fed1beaff2137828b29
Ivan Tkachyshyn <itkachyshyn@apreel.com>
2020-04-27 20:07:46 +0200

    chore: PKOU-1179

--- src/app/claims/children/messages/components/messenger/messenger.component.ts
+++ src/app/claims/children/messages/components/messenger/messenger.component.ts
@@ -6,7 +6,8 @@ import {
     EventEmitter,
     ViewChild,
     ElementRef,
-    NgZone
+    NgZone,
+    AfterViewInit
 } from '@angular/core';
 
 import { DraftMessage, Message } from '@pko/claims/models';
@@ -17,8 +18,9 @@ import { DraftMessage, Message } from '@pko/claims/models';
     styleUrls: ['./messenger.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessengerComponent {
+export class MessengerComponent implements AfterViewInit {
     @Input() focusMessageInput: boolean;
+    @Input() scrollToBottom: boolean;
 
     @Input()
     set messages(messages: Array<Message>) {
@@ -32,12 +34,25 @@ export class MessengerComponent {
     @Output() sendMessage: EventEmitter<DraftMessage> = new EventEmitter<DraftMessage>();
 
     @ViewChild('scrollable', { static: true, read: ElementRef }) scrollable: ElementRef;
+    @ViewChild('messenger', { static: true, read: ElementRef }) messenger: ElementRef;
 
     private _messages: Array<Message>;
 
     constructor(private _ngZone: NgZone) { }
 
     trackByMessage = (index: number, message: Message) => message.postDate;
+
+    ngAfterViewInit(): void {
+        if (this.scrollToBottom) {
+            this._ngZone.runOutsideAngular(() =>
+                Promise.resolve().then(() =>
+                    this.messenger.nativeElement.scrollIntoView({
+                        block: 'end'
+                    })
+                )
+            );
+        }
+    }
 
     private _scrollToBottom(): void {
         if (this.scrollable) {
