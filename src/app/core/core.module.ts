6c4bbc233c62efae92379ee769130402da2ff508
Ivan Tkachyshyn <itkachyshyn@apreel.com>
2020-05-06 22:08:48 +0200

    chore: translation config

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,31 +1,17 @@
 import { LOCALE_ID, NgModule, Optional, SkipSelf } from '@angular/core';
 import { HashLocationStrategy, LocationStrategy, registerLocaleData } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { AngularSvgIconModule } from 'angular-svg-icon';
 
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslationConfigModule } from '@pko/translation';
 
 import localePl from '@angular/common/locales/pl';
 
 registerLocaleData(localePl, 'pl');
 
-export function defaultTranslationLoader(http: HttpClient): TranslateHttpLoader {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
-
 @NgModule({
     imports: [
         AngularSvgIconModule.forRoot(),
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: defaultTranslationLoader,
-                deps: [ HttpClient ]
-            },
-            defaultLanguage: 'pl',
-            useDefaultLang: true
-        })
+        TranslationConfigModule
     ],
     providers: [
         { provide: LocationStrategy, useClass: HashLocationStrategy },
---
+++ src/app/translation/index.ts
@@ -0,0 +1,1 @@
+export { TranslationConfigModule } from './translation.module';
